f70ff0f5553f6a678a57418447be054f884b969f
kabishou11 <123629497+kabishou11@users.noreply.github.com>
2025-03-18 23:57:55 +0800

    Create setup.py
    解决了No module named 'ragflows'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ragflows",
+    version="0.1.0",
+    packages=['ragflows'],
+    package_dir={'ragflows': 'ragflows'},
+    install_requires=[
+        # 添加项目依赖（如果有）
+    ],
+)
